74020667a68ef34eaa7d2b25ac2aa347a3a4af58
Able <ablekilinda@gmail.com>
2022-11-07 17:17:03 +0300

    adding asite

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,19 @@
-<div class="menu-btn">
-   <i class=" fas fa-bars"></i>
-</div>
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title>Admin Site</title>
+   <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+   <div class="menu-btn">
+      <i class="fas fa-bars"></i>
+   </div>
+
+   <div class="side-bar">
+      
+   </div>
+
+</body>
+</html>
