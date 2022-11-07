829750eff524c9c53f2c002c927400e47d9c80a6
Able <ablekilinda@gmail.com>
2022-11-07 16:06:39 +0300

    adding style

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,29 +1,3 @@
-<?php
- require 'header.php';
-?>
-
-<?php
-         if (isset($_SESSION['userId'])){
-            echo "<p>You are logged in!</p>";
-         }
-         else {
-            echo "<p>You are logged out!</p>";
-         }
-      ?>
-     
-
-     <?php
-                        if (isset($_SESSION['userId'])){
-                           echo '<form action="../backend/logout-inc.php" method="post">
-                           <button name="logout-submit" type="submit">Logout</button>
-                       </form>';
-                        }
-                        else {
-                           echo '<form action="backend/login-inc.php" method="post">
-                           <input type="text" name="mailuid" id="" placeholder="Username/E-mail..">
-                           <input type="password" name="pwd" placeholder="Password..">
-                           <button name="login-submit" type="submit">Login</button>
-                       </form>
-                       <a href="signup.php">Signup</a>';
-                        }      
-                ?>
+<div class="menu-btn">
+   <i class=" fas fa-bars"></i>
+</div>
