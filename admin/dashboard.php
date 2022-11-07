52160243aadfb1a5af022d4d7114dfdb1faf99c1
Able <ablekilinda@gmail.com>
2022-11-07 17:41:16 +0300

    adding a sidebar

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,17 +12,18 @@
    </div>
 
    <div class="side-bar">
-   <header>
-      <div class="close-btn">
-         <i class="fas fa-times"></i>
-      </div>
-      <img src="../images/banner8.jpg" alt="">
-      <h1>Admin Board</h1>
-   </header>
+      <header>
+         <div class="close-btn">
+            <i class="fas fa-times"></i>
+         </div>
+         <img src="../images/banner8.jpg" alt="">
+         <h1>Admin Board</h1>
+      </header>
 
    </div>
 
 
 
 </body>
+
 </html>
