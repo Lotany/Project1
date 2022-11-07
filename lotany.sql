e7e7651692e267e6293db437451a91548e0aada9
Able <ablekilinda@gmail.com>
2022-11-07 18:29:15 +0300

    adding admin panel

--- lotany.sql
+++ lotany.sql
@@ -1,7 +1,8 @@
 Create table users(
     id int (11) not null auto_increment primary key,
     username varchar(20) not null,
-    email varchar(30) not null
+    email varchar(30) not null,
+    password varchar(100) not null
 );
 
 Create table testimonies(
@@ -11,3 +12,17 @@ Create table testimonies(
     image varchar(300)
 );
 
+Create table banner(
+    id int(10) not null auto_increment primary key,
+    header varchar(200) not null,
+    content varchar (300) not null,
+    dateposted timestamp
+);
+
+
+Create table messages(
+    id int(11) not null auto_increment primary key,
+    name varchar(40) not null,
+    email varchar(100) not null,
+    message varchar(400) not null
+);
