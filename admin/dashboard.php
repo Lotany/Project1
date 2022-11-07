626e8ec8988db68d4a38ff8f53592c36012d5e29
Able <ablekilinda@gmail.com>
2022-11-07 18:08:27 +0300

    adding admin site

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -35,7 +35,21 @@
    </div>
 
 
+<script>
+   $(document).ready(function(){
 
+      $('.menu-btn').click(function(){
+
+         $('.side-bar').addClass('active');
+         $('.menu-btn').css("visibility","hidden");
+      });
+
+      $('.close-btn').click(function(){
+         $('.side-bar').removeClass('active');
+         $('.menu-btn').css("visibility", "visible");
+      });
+   });
+</script>
 </body>
 
 </html>
