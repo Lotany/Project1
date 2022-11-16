3f82203c1b876c0fb563892dbf0973b87af8ac4a
Able <ablekilinda@gmail.com>
2022-11-16 06:14:45 +0300

    adding astyle

--- detail.php
+++ detail.php
@@ -3,19 +3,17 @@
         <!--Main-->
          <main>
             <section class="case-vid">
-                <div class="wrapper">
                     <h2>Project 1</h2>
           <video src="" autoplay poster="" controls></video>  
           <!--  <img src="images/spartans-what-is-your-profession-300.gif" alt="300 spartans"> -->
            
           <article>
-            <h3>Full stack</h3>
+            <h3><a href="#">View on Github</a></h3>
             <div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, consequatur amet? Expedita dolorem architecto tempora cupiditate officia perspiciatis voluptatibus excepturi debitis eaque iure. Delectus quis mollitia modi ipsa minus libero. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam repellat a cupiditate mollitia quisquam doloremque maxime est dicta quos pariatur optio dolores, itaque, asperiores laboriosam perferendis saepe delectus reiciendis ducimus? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi soluta tenetur enim quas cupiditate temporibus neque? Labore consequuntur minima consequatur modi. Dolorem, eius? Ipsa quasi a vero sed est! Dignissimos.
                 </p>
             </div>
           </article>
-                </div>
             </section>
          </main>
 
