412ac1459508258705cf249eedcc50cd806ca997
Able <ablekilinda@gmail.com>
2022-11-16 04:59:32 +0300

    adding a case

--- cases.php
+++ cases.php
@@ -12,22 +12,16 @@
             <a href="detail.php">
                 <div class="cases-link">
                     <h3>Mnazi</h3>
-                    <div class="row">
-                        <div class="column">
+
                             <img src="images/banner5.png" style="width:100%">
-                        </div>
-                    </div>
                 </div>
             </a>
 
             <a href="detail.php">
                 <div class="cases-link">
                     <h3>Farm Management</h3>
-                    <div class="row">
-                        <div class="column">
+
                             <img src="images/banner5.png" style="width:100%">
-                        </div>
-                    </div>
                 </div>
             </a>
 
@@ -35,11 +29,8 @@
             <a href="detail.php">
                 <div class="cases-link">
                     <h3>Riz bakers</h3>
-                    <div class="row">
-                        <div class="column">
                             <img src="images/banner5.png" style="width:100%">
-                        </div>
-                    </div>
+                
                 </div>
             </a>
 
