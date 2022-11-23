2e4ae7464b21714e59a0576e7b3631542323db61
Able <ablekilinda@gmail.com>
2022-11-23 07:39:54 +0300

    adding admin  site

--- admin/header.php
+++ admin/header.php
@@ -1,38 +1,39 @@
 <!DOCTYPE html>
 <html>
-   <head>
-      <title>Daashboard</title>
-      <link rel="stylesheet" href="style.css">
+
+<head>
+    <title>Daashboard</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<nav>
-<ul class="menu">
-    <li class="logo">
-        <a href="index.php">Admin</a>
-    </li>
-    <li class="item">
-        <a href="#">Home</a>
-    </li>
-    <li class="item">
-        <a href="#">About</a>
-    </li>
-    <li class="item has-submenu">
-        <a tabindex="0">Services</a>
-           <ul class="submenu">
-            <li class="subitem"><a href="#">Development</a></li>
-            <li class="subitem"><a href="#">Design</a></li>
-            <li class="subitem"><a href="#">Networking</a></li>
-            <li class="subitem"><a href="#">Repair & Maintenance</a></li>
-           </ul>
-    </li>
+    <nav>
+        <ul class="menu">
+            <li class="logo">
+                <a href="dashboard.php">Admin</a>
+            </li>
+            <li class="item">
+                <a href="#">Projects</a>
+            </li>
+            <li class="item">
+                <a href="#">Testimonies</a>
+            </li>
+
+            <li class="item">
+                <a href="#">Messages</a>
+            </li>
 
 
-     <li class="item">
-        <a href="#">Contact</a>
-      </li>
+            <li class="item">
+                <a href="#">Services</a>
+            </li>
 
-      <li class="toggle">
-        <a href="#"><i class="fas fa-bars"></i></a>
-      </li>
-</ul>
-</nav>
+            <li class="item">
+                <a href="#">Banner</a>
+            </li>
+
+            <li class="toggle">
+                <a href="#"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+    </nav>
