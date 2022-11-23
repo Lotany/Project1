e53bfe84c0ff2fe63416a7ac80fd42c499c2d755
Able <ablekilinda@gmail.com>
2022-11-23 08:28:30 +0300

    adding a site

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,11 +1,29 @@
 <?php require 'header.php' ?>
 <div class="wrapper">
    <div class="pro">
-      <h2>Projects</h2>
+      <h2 class="page-title">Manage Projects</h2>
+      <table>
+         <thead>
+            <th>SN</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th colspan="3">Action</th>
+         </thead>
+         <tbody>
+            <tr>
+               <td>1</td>
+               <td>This is the first post</td>
+               <td>Awa</td>
+               <td><a href="#" class="edit">edit</a></td>
+
+               <td><a href="#" class="delete">delete</a></td>
+               <td><a href="#" class="publish">publish</a></td>
+            </tr>
+         </tbody>
+      </table>
    </div>
 
    <div class="test">
-      <h2>Testimonials</h2>
    </div>
 
    <div class="message">
