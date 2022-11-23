20a9189642f7c025ab62b4f92667b9fff2731551
Able <ablekilinda@gmail.com>
2022-11-23 10:08:11 +0300

    adding style

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
         <meta charset="utf-8">
         <title>Able Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="">
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     </head>
