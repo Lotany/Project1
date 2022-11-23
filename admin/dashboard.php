1012a07ab869d3c01bdeb6fe19cfae4224014521
Able <ablekilinda@gmail.com>
2022-11-23 08:15:49 +0300

    adding admin syle

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,10 +1,26 @@
 <?php require 'header.php' ?>
 <div class="wrapper">
    <div class="pro">
-   <h2>Projects</h2>
-   
+      <h2>Projects</h2>
+   </div>
+
+   <div class="test">
+      <h2>Testimonials</h2>
+   </div>
+
+   <div class="message">
+      <h2>Messages</h2>
+   </div>
+
+   <div class="service">
+      <h2>Services</h2>
+   </div>
+
+   <div class="banner">
+      <h2>Banner</h2>
    </div>
 
 </div>
 </body>
+
 </html>
